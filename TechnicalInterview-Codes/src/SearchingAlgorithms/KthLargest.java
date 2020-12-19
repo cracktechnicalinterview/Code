ccb5fe1f013e94424febb7b94e674aebaeaa4dd2
Anindya (Technical Interview) <68023949+cracktechnicalinterview@users.noreply.github.com>
2020-12-20 01:49:08 +0530

    Kth Largest or Smallest Number in an array

---
+++ TechnicalInterview-Codes/src/SearchingAlgorithms/KthLargest.java
@@ -0,0 +1,33 @@
+package PriorityQueue;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthLargest {
+	
+	public static int findKMaximum(int arr[],int k) {
+		int max=0;
+		PriorityQueue<Integer> q=new PriorityQueue<Integer>(Collections.reverseOrder());
+		
+		for(int i=0;i<arr.length;i++) {
+			if(!q.contains(arr[i]))
+				q.add(arr[i]);
+		}
+		
+		while(k!=0) {	
+			max=q.poll();
+			k--;
+		}
+
+		return max;
+	}
+
+	public static void main(String[] args) {
+		
+		int arr[]= {7, 4, 6, 3, 9, 1, 9};
+		System.out.println(KthLargest.findKMaximum(arr, 2));
+		
+
+	}
+
+}
