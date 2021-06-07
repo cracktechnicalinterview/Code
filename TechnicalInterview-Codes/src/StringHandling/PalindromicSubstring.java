b11a6a241d6f3d6ab154dded1132f448890e9af0
Anindya Sankar Dasgupta <anindya.s.dasgupta@oracle.com>
2021-06-08 00:56:00 +0530

    Adding brute force solution

--- TechnicalInterview-Codes/src/StringHandling/PalindromicSubstring.java
+++ TechnicalInterview-Codes/src/StringHandling/PalindromicSubstring.java
@@ -4,6 +4,29 @@ import java.util.HashSet;
 
 public class PalindromicSubstring {
 
+	// BRUTE FORCE ////////
+	public static boolean isPalindrome(String str) {
+		return str.equalsIgnoreCase(new StringBuilder(str).reverse().toString());
+	}
+
+	public static void generateAllSubstringBruteForce(String str) {
+		HashSet<String> set = new HashSet<String>();
+		int maxLength = 0;
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i; j < str.length(); j++) {
+				if (isPalindrome(str.substring(i, j+1))) {
+					set.add(str.substring(i, j+1));
+					maxLength = Math.max(maxLength, str.substring(i, j+1).length());
+				}
+			}
+		}
+
+		System.out.println("Brute Force (Max Length)==> " + maxLength);
+		System.out.println("Brute Force (Palindromic Substring) ==>"+set);
+		
+	}
+
+	/// OPTIMIZED` /////
 	static int maxLength = 0;
 
 	public static void isPalindrome(String str, int left, int right, HashSet<String> set) {
@@ -31,12 +54,15 @@ public class PalindromicSubstring {
 
 		}
 		System.out.println(set);
-		System.out.println("Longest Palindromic Substring = " +maxLength);
+		System.out.println("Longest Palindromic Substring = " + maxLength);
 	}
 
 	public static void main(String[] args) {
 		String s = "google";
 		generateAllSubstring(s);
+		
+		System.out.println();
+		generateAllSubstringBruteForce(s);
 
 	}
 
