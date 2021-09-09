b8d331e6cec0bdc5683a81f57017b1a4c6aabeee
Anindya Sankar Dasgupta <anindya.s.dasgupta@oracle.com>
2021-09-09 11:01:52 +0530

    Singelton Design Pattern

---
+++ TechnicalInterview-Codes/src/DesignPattern/SingletonMain.java
@@ -0,0 +1,37 @@
+package DesignPattern;
+
+public class SingletonMain {
+
+	static class Singleton {
+		private static Singleton instance;
+
+		private Singleton() {}
+
+		public static synchronized Singleton getInstance() {
+			
+			if (instance == null) {
+				instance=new Singleton();
+				System.out.println("Object getting created");
+			}
+			return instance;
+		}
+		
+		public void doSomeOperation() {
+			System.out.println("Do some operation");
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		Singleton instance=Singleton.getInstance();
+		instance.doSomeOperation();
+		
+		Singleton instance2=Singleton.getInstance();
+		instance2.doSomeOperation();
+		
+		
+		
+	}
+
+}
